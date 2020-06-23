1b3ba3f4569ec56a9c100c8051c908dd22af7fc3
Liao <w09032222@gmail.com>
2020-06-24 06:02:30 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
-// // import jQuery
-// let script = document.createElement("script");
-// script.src =
-//   "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-// script.type = "text/javacript";
-// document.getElementsByTagName("head")[0].appendChild(script);
-// // end of import
-
 // Initialize
+//dat.gui
 let guiControls = function () {
-  this.background = "#c8c8c8";
+  this.BodyColor = "#c8c8c8";
+  this.DivColor = "#080808";
+  this.TextColor = "#fefefe";
 };
 let controls = new guiControls();
 
+//css
 $(document).ready(() => {
+  //*
   $("*").css("margin", "0");
+  //body
   $("body").css({
-    background: controls.background,
+    background: controls.BodyColor,
     width: "100vw",
     height: "100vh",
+    overflow: "hidden",
   });
+
+  //div#a
+  $("body").append(" <div></div> ");
+  $("div").attr({ id: "a" });
+  $("div#a").css({
+    margin: "calc(50vh - 50px) calc(50vw - 50px)",
+    "line-height": "100px",
+    width: "100px",
+    height: "100px",
+    background: controls.DivColor,
+  });
+
+  //p#text
+  $("div#a").append(" <p>Text</p> ");
+  $("p").attr({ id: "text" });
+  $("#text").css({
+    "font-size": "1.5em",
+    color: controls.TextColor,
+    "text-align": "center",
+  });
+  //end of css
+
+  //dat.gui
   let gui = new dat.GUI();
-  gui.addColor(controls, "background");
+  gui.addColor(controls, "BodyColor");
+  gui.addColor(controls, "DivColor");
+  gui.addColor(controls, "TextColor");
 });
 
 //render
 let render = function () {
-  $("body").css("background", controls.background);
+  $("body").css("background", controls.BodyColor);
+  $("div#a").css("background", controls.DivColor);
+  $("#text").css("color", controls.TextColor);
 };
 
 let renderer = window.setInterval(() => render(), 1000 / 60);
